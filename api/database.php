945639060276526fbf9a4fda14ba54485cc92781
Youssef-codin <youssef240100538@sut.edu.eg>
2025-11-25 19:20:57 +0200

    Working on backedn

---
+++ api/database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database
+{
+    private $host = 'localhost';
+    private $db_name = 'expense_tracker';
+    private $username = 'root';
+    private $password = '';
+
+    private static $instance = null; // The single instance
+    private $conn;
+
+    private function __construct()
+    {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4";
+
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            header('Content-Type: application/json');
+            http_response_code(500);
+            echo json_encode(["success" => false, "message" => "DB Connection Error: " . $e->getMessage()]);
+            exit;
+        }
+    }
+
+    public static function connect()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance->conn;
+    }
+
+    public static function query($sql, $params = [])
+    {
+        $stmt = self::connect()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+}
